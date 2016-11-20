cfaee263d6735737f90ab51db82cbbad54c7ace7
Thomas Mao <ccm29cam@gmail.com>
2016-11-20 11:03:45 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 2017 MakeNTU website
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__),"")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render("index.html")
+
+app = webapp2.WSGIApplication([
+    ('/',MainPage),
+    ],
+    debug = True
+    )
